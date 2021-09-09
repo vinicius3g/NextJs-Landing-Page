e5469ef5b20ac2409ea6fa0a61ed352c240a0009
vinicius.paixao <vinicius.paixao@acct.global>
2021-09-08 21:14:51 -0300

    types

--- src/types/api.ts
+++ src/types/api.ts
@@ -49,10 +49,89 @@ export type SectionConceptsProps = {
   }>
 }
 
+export type Modules = {
+  title: string
+  subtitle: string
+  description: string
+}
+
+export type SectionModulesProps = {
+  title: string
+  modules: Modules[]
+}
+
+export type SectionAgendaProps = {
+  id: string
+  title: string
+  description: string
+}
+
+export type PricingBoxProps = {
+  id: string
+  totalPrice: number
+  numberInstallments: number
+  priceInstallment: number
+  benefits: string
+  button: {
+    id: string
+    label: string
+    url: string
+  }
+}
+
+export type SocialLinks = {
+  title: string
+  url: string
+}
+
+export type Author = {
+  name: string
+  role: string
+  description: string
+  photo: {
+    url: string
+    alternativeText: string
+  }
+  socialLinks: SocialLinks[]
+}
+
+export type SectionAboutUsProps = {
+  id: string
+  title: string
+  authors: Author[]
+}
+
+export type Reviews = {
+  name: string
+  text: string
+  photo: {
+    url: string
+  }
+}
+export type SectionReviewsProps = {
+  id: string
+  title: string
+  reviews: Reviews[]
+}
+export type SectionFaqProps = {
+  id: number
+  title: string
+  questions: Array<{
+    id: number
+    question: string
+    answer: string
+  }>
+}
 export type LandingPageProps = {
   logo: LogoProps
   header: HeaderProps
   sectionAboutProject: SectionAboutProps
   sectionTech: SectionTechProps
   sectionConcepts: SectionConceptsProps
+  sectionModules: SectionModulesProps
+  sectionAgenda: SectionAgendaProps
+  pricingBox: PricingBoxProps
+  sectionAboutUs: SectionAboutUsProps
+  sectionReviews: SectionReviewsProps
+  sectionFaq: SectionFaqProps
 }
